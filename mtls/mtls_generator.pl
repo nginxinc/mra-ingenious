b7c288fe42fa96e6c67112a579ae066cd8f3a4bf
cstetson <chris.stetson@nginx.com>
2018-06-10 21:19:48 -0700

    Working implementation of mtls across the MRA
    - mtls_generator.pl does the work of generating all of the related certs in a Certificate Authority manner so each service has signing and authenticating capabilities
    - ca.conf which has all of the Certificate Authority configurations needed to properly generate CA signed certs

--- mtls/mtls_generator.pl
+++ mtls/mtls_generator.pl
@@ -5,6 +5,11 @@ use Data::Dumper qw(Dumper);
 
 print `pwd` . "\n";
 
+`rm ./mtls/index.txt* ./mtls/serial && touch ./mtls/index.txt && echo '01' > ./mtls/serial`;
+unless (-e "./mtls/serial")
+{
+    `echo '01' > ./mtls/serial`;
+}
 my %services;
 
 $services{'resizer'} = { directory => "mra-photoresizer/nginx/ssl"}; 
@@ -24,33 +29,46 @@ foreach my $key (keys %services)
     print "We are dealing with $key and putting the CA certs and keys into the '$directory' directory\n";
     #print Dumper \%service;
     my $opensslCommand = "openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \\
-        -subj  \"/C=US/ST=California/L=San Francisco/O=NGINX/OU=Professional Services/CN=$key\" \\
+        -subj  \"/C=US/ST=California/L=San Francisco/O=NGINX/OU=Professional Services/CN=$key" . "_ca\" \\
         -keyout $directory/$key" . "_ca.key \\
         -out $directory/$key". "_ca.pem";
     unless (-e "$directory/$key". "_ca.pem")
     {
-        print $opensslCommand . "\n";
-        `$opensslCommand`;
+        formatRun($opensslCommand);
     }
     $opensslCommand = "openssl req -new \\
         -subj \"/C=US/ST=California/L=San Francisco/O=NGINX/OU=Professional Services/CN=$key\" \\
-        -key $directory/$key" . "_ca.key \\
+        -newkey rsa:4096 -nodes \\
+        -keyout $directory/$key" . ".key \\
         -out $directory/$key" . ".csr";
     unless (-e "$directory/$key". ".csr")
     {
-        print $opensslCommand . "\n";
-        `$opensslCommand`;
+        formatRun($opensslCommand);
     }
     $opensslCommand = "openssl x509 -req -days 365 \\
-        -in $directory/$key" . ".csr \\
+        -in $directory/$key" . ".csr \\ 
         -CA $directory/$key" . "_ca.pem \\
         -CAkey $directory/$key" . "_ca.key \\
         -set_serial 01 \\
-        -out $directory/$key" . ".pem";
-    unless (-e "$directory/$key". ".pem")
+        -out $directory/$key" . "_ss.pem";
+    unless (-e "$directory/$key". "_ss.pem")
     {
-        print $opensslCommand . "\n";
-        `$opensslCommand`;
+        formatRun($opensslCommand);
+    }
+    if (-e "$directory/$key". "_ss.pem")
+    {
+        my $outDirectory =  $service{'directory'};
+        # the ca -out option requires that no "./" be prepended
+        $opensslCommand = "openssl ca -batch \\
+            -cert $directory/$key" . "_ca.pem \\
+            -keyfile $directory/$key" . "_ca.key \\
+            -config ./mtls/ca.conf  \\
+            -out $outDirectory/$key" . ".pem \\
+            -ss_cert $directory/$key" . "_ss.pem \\
+            -infiles $directory/$key" . ".csr";
+        formatRun($opensslCommand);
+        my $selfSignedCert = "$directory/$key" . "_ss.pem";
+        `rm $selfSignedCert`;
     }
     print "Done with $key \n";
 }
@@ -68,22 +86,42 @@ foreach my $key (keys %services)
     {
         my $serviceIndex = "service" . $index;
         my $connectedService =  $service{$serviceIndex};
+        print "The connected service is: $connectedService \n";
         my %connectedServiceInfo =  %{$services{$connectedService}};
+        my $connectedServiceName =  $service{$serviceIndex} . "_" . $key;
         my $connectedServiceDirectory = "./" . $connectedServiceInfo{"directory"};
         my $opensslCommand = "openssl req -new \\
-            -subj \"/C=US/ST=California/L=San Francisco/O=NGINX/OU=Professional Services/CN=$connectedService\" \\
-            -key $connectedServiceDirectory/$connectedService" . "_ca.key \\
+            -subj \"/C=US/ST=California/L=San Francisco/O=NGINX/OU=Professional Services/CN=$connectedServiceName\" \\
+            -newkey rsa:4096 -nodes -keyout $serviceDirectory/$connectedService" . "_client.key \\
             -out $serviceDirectory/$connectedService" . "_client.csr";
-        print "$opensslCommand\n";
-        `$opensslCommand`;
-        $opensslCommand = "openssl x509 -req -days 365 \\
-            -in $serviceDirectory/$connectedService" . "_client.csr \\
-            -CA $connectedServiceDirectory/$connectedService" . "_ca.pem \\
-            -CAkey $connectedServiceDirectory/$connectedService" . "_ca.key \\
-            -set_serial 01 \\
-            -out $serviceDirectory/$connectedService" . "_client.pem";
-        print $opensslCommand . "\n";
-        `$opensslCommand`;
+        unless (-e "$serviceDirectory/$connectedService" . "_client.csr")
+        {
+            formatRun($opensslCommand);
+        }
+        $opensslCommand = "openssl x509 -req -days 365 -in $serviceDirectory/$connectedService" . "_client.csr \\
+             -CA $connectedServiceDirectory/$connectedService" . "_ca.pem  \\
+             -CAkey $connectedServiceDirectory/$connectedService" . "_ca.key \\
+             -set_serial 01 \\
+             -out $serviceDirectory/$connectedService" . "_client_ss.pem";
+        unless (-e "$serviceDirectory/$connectedService" . "_client_ss.pem")
+        {
+            formatRun($opensslCommand);
+        }
+        if (-e "$serviceDirectory/$connectedService" . "_client_ss.pem")
+        {
+            my $outDirectory =  $service{'directory'};
+            # the ca -out option requires that no "./" be prepended
+            $opensslCommand = "openssl ca -batch \\
+                -cert $connectedServiceDirectory/$connectedService" . "_ca.pem \\
+                -keyfile $connectedServiceDirectory/$connectedService" . "_ca.key \\
+                -config ./mtls/ca.conf \\
+                -out $outDirectory/$connectedService" . "_client.pem \\
+                -ss_cert $serviceDirectory/$connectedService" . "_client_ss.pem \\
+                -infiles $serviceDirectory/$connectedService" . "_client.csr";
+            formatRun($opensslCommand);
+            my $selfSignedCert = "$serviceDirectory/$connectedService" . "_client_ss.pem";
+            `rm $selfSignedCert`;
+        }
         my $copyCommand; 
         unless (-e "$connectedServiceDirectory/$key" . "_ca.pem")
         {
@@ -97,4 +135,13 @@ foreach my $key (keys %services)
         }
         $index++;
     }
+}
+
+sub formatRun 
+{
+    my $opensslCommand = $_[0];
+    $opensslCommand =~ s/\\ *\n//g;
+    $opensslCommand =~ s/  +/ /g;
+    print $opensslCommand . "\n";
+    `$opensslCommand`;
 }
